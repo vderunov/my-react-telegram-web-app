33d570266323208b1ab1ae8ca4f3d54c2cc58cee
Volodymyr Derunov <vderunov@gmail.com>
2024-08-17 00:56:22 +0300

    Remove alias configuration in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,22 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import basicSsl from '@vitejs/plugin-basic-ssl';
-import path from 'node:path';
-import { fileURLToPath } from 'node:url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), basicSsl()],
-  resolve: {
-    alias: {
-      '@web3modal/core': path.resolve(
-        __dirname,
-        'node_modules/@web3modal/core'
-      ),
-    },
-  },
   build: {
     outDir: './docs',
     rollupOptions: {
